e00f887f2e036612c1a964612d6e371cbecde6e8
Rei <gjoka.gerti@external.toyota-europe.com>
2023-08-04 17:44:50 +0200

    laplacian-alg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect, useRef } from 'react';
 import Webcam from 'react-webcam';
 import * as tf from '@tensorflow/tfjs';
-import { drawCircle, draw } from './utilities'
-
-
-const SCORE = 0.8
-
-const windowWidth = window.innerWidth
-const windowHeight = window.innerHeight
-
-const CAMERA_RESOLUTION_WIDTH = 1280
-const CAMERA_RESOLUTION_HEIGHT = 720
-
-const CAMERA_CENTER_X = CAMERA_RESOLUTION_WIDTH / 2
-const CAMERA_CENTER_Y = CAMERA_RESOLUTION_HEIGHT / 2
+import { isWithinCirclePerimeter, getPictureResult, draw } from './utilities'
 
 const CIRCLE_WIDTH = 300
 const CIRCLE_HEIGHT = 300 
 
 const MESSAGE_NOT_FOUND = 'Face not detected!'
-const MESSSAGE_FOUND = 'Success! Face detected!'
+const MESSAGE_FOUND = 'Success! Face detected!'
+
+const DETECTION_INTERVAL = 500
+const SHOW_DETECTION_BOX = true
+let engine
 
 function App() {
   const webcamRef = useRef(null);
@@ -30,30 +23,28 @@ function App() {
   const [canvasLeft, setCanvasLeft] = useState(0)
   const [canvasTop, setCanvasTop] = useState(0)
   const [message, setMessage] = useState('')
-  console.log(111, canvasTop, canvasLeft)
+  const [resolution, setResolution] = useState(null)
+  const [cameraCenter, setCameraCenter] = useState(null)
+  console.log(111, { resolution })
 
-  const runFacedetection = async () => {
-
+  const runFaceDetection = async () => {
     const model = await blazeface.load()
-    console.log("FaceDetection Model is Loaded..")
-    setInterval(() => {
+    engine = setInterval(() => {
       detect(model);
-    }, 500);
-
+    }, DETECTION_INTERVAL);
   }
-
-
-  const returnTensors = false;
+  console.log(111, 'init', webcamRef.current)
 
   useEffect(() => {
     // Function to initialize the webcam stream
     const initializeWebcam = async () => {
       try {
         const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-
         const videoTrack = stream.getVideoTracks()[0];
         const settings = videoTrack.getSettings();
         const resolution = { width: settings.width, height: settings.height };
+        setResolution(resolution)
+        setCameraCenter({ x: resolution.width/2, y: resolution.height/2 })
 
         // Set the stream as the video element's source
         if (videoRef.current) {
@@ -68,13 +59,22 @@ function App() {
 
     // Clean up the stream when the component unmounts
     return () => {
-      if (videoRef.current && videoRef.current.srcObject) {
-        const stream = videoRef.current.srcObject;
-        const tracks = stream.getTracks();
-        tracks.forEach((track) => track.stop());
-      }
+      terminate()
     };
   }, []);
+
+  const stopFaceDetection = () => {
+    clearInterval(engine)
+  }
+
+  const terminate = () => {
+    stopFaceDetection()
+    if (videoRef.current && videoRef.current.srcObject) {
+      const stream = videoRef.current.srcObject;
+      const tracks = stream.getTracks();
+      tracks.forEach((track) => track.stop());
+    }
+  }
 
   const detect = async (model) => {
     if (
@@ -97,7 +97,7 @@ function App() {
 
       // Make detections
 
-      const predictions = await model.estimateFaces(video, returnTensors);
+      const predictions = await model.estimateFaces(video, false);
 
       const canvasLeftX = canvasRef?.current.getBoundingClientRect().left;
       setCanvasLeft(canvasLeftX)
@@ -108,52 +108,51 @@ function App() {
       const allowedRightX = circleRef?.current?.getBoundingClientRect().right
       const allowedTopY = circleRef?.current?.getBoundingClientRect().top
       const allowedBottomY = circleRef?.current?.getBoundingClientRect().bottom
-
-      console.log(111, { allowedRightX })
      
       const ctx = canvasRef.current.getContext("2d");
-      draw(predictions, ctx)
-      const prediction = predictions[0]
-      console.log(111, prediction?.bottomRight[1]??-1)
-      
-      if (prediction?.topLeft && prediction.bottomRight) {
-
-        if (prediction.topLeft[0] + canvasLeftX >= allowedLeftX // Left X
-          && prediction.bottomRight[0] <= allowedRightX // Right X
-          && prediction.topLeft[1] + canvasTopY >= allowedTopY // Top Y
-          && prediction.bottomRight[1] <= allowedBottomY) { // Bottom Y
-
-          console.log("Success! Face detected!")
-          setMessage(MESSSAGE_FOUND)
-
-        } else {
-          console.log('Face not found!')
-          setMessage(MESSAGE_NOT_FOUND)
-        }
+      if (SHOW_DETECTION_BOX) {
+        draw(predictions, ctx)
       }
-
-
+      const prediction = predictions[0]
+      console.log(111,{ prediction })
+      const faceDetected = isWithinCirclePerimeter(prediction,  canvasTopY, allowedTopY, allowedRightX, allowedBottomY, allowedLeftX, canvasLeftX)
+     
+      if (faceDetected) {
+        console.log("Success! Face detected!")
+        setMessage(MESSAGE_FOUND)
+        const picture = webcamRef.current.getScreenshot()
+        getPictureResult(picture)
+      } else {
+        console.log('Face not found!')
+        setMessage(MESSAGE_NOT_FOUND)
+      } 
 
     }
 
   }
 
   useEffect(() => {
-    runFacedetection();
+    runFaceDetection();
   }, [])
 
 
   return (
     <div style={{
+      position: 'relative', 
+    }}>
+    <div id='overlay' style={{
       background: 'black',
       width: '100%',
-      height: '100vh'
+      height: '100vh',
+      position:'relative'
     }}>
-      <Webcam
+      {resolution && cameraCenter && (
+        <>
+        <Webcam
         ref={webcamRef}
         videoConstraints={{
-          width: CAMERA_RESOLUTION_WIDTH,
-          height: CAMERA_RESOLUTION_HEIGHT,
+          width: resolution.width,
+          height: resolution.height,
           facingMode: "user"
         }}
         style={{
@@ -166,8 +165,8 @@ function App() {
           bottom: 0,
           textAlign: "center",
           zIndex: 9,
-          width: CAMERA_RESOLUTION_WIDTH,
-          height: CAMERA_RESOLUTION_HEIGHT,
+          width: resolution.width,
+          height: resolution.height,
           border: '2px solid yellow'
         }}
       />
@@ -184,38 +183,47 @@ function App() {
           bottom: 0,
           textAlign: "center",
           zIndex: 9,
-          width: CAMERA_RESOLUTION_WIDTH,
-          height: CAMERA_RESOLUTION_HEIGHT,
-          border: '2px solid red'
+          width: resolution.width,
+          height: resolution.height,
+          border: '2px solid red',
         }}
-      />
-
-        <p style={{
+      /> 
+ 
+       {webcamRef?.current && (
+         <div style={{
           position: 'absolute',
-          top: 100 ,
-          left: canvasLeft + CAMERA_CENTER_X - (CIRCLE_WIDTH / 2) + 100,
+          top: 20,
+          left: canvasLeft + cameraCenter.x - (CIRCLE_WIDTH / 2),
+          width: CIRCLE_WIDTH,
           zIndex: 15,
-          background: 'black',
-          color: "white",
-          borderRadius: '10px',
-          padding: '4px 8px'
-        }}>{message}</p>
+          padding: '4px 8px',
+          textAlign: 'center'
+        }}>
+          <p style={{ background: 'gray', display: 'inline-block', padding: '4px 8px',borderRadius: '10px',color: "white"}}>{message}</p>
+        </div>
+       )}
 
-      <div
+      {webcamRef?.current && (
+        <div
         ref={circleRef}
         style={{
           position: "absolute",
-          top: canvasTop + CAMERA_CENTER_Y - (CIRCLE_HEIGHT / 2),
-          left: canvasLeft + CAMERA_CENTER_X - (CIRCLE_WIDTH / 2),
+          top: canvasTop + cameraCenter?.y - (CIRCLE_HEIGHT / 2),
+          left: canvasLeft + cameraCenter.x - (CIRCLE_WIDTH / 2),
           right: 0,
           bottom: 0,
-          zIndex: 10,
+          zIndex: 100,
           border: '2px solid blue',
           width: CIRCLE_WIDTH,
           height: CIRCLE_HEIGHT,
-          borderRadius: '100%'
+          borderRadius: '100%',
+          boxShadow: '0 0 0 1000px rgba(0, 0, 0, 0.7)'
         }}
       />
+      )}
+        </>
+      )}
+    </div>
     </div>
   );
 
--- src/utilities.js
+++ src/utilities.js
@@ -25,3 +25,116 @@ export function drawCircle(ctx, centerX, centerY, radius) {
   ctx.fillStyle = 'blue'; // Change color if desired
   ctx.fill();
 }
+
+export function getPictureResult(base64String) {
+  const src = blobUrl(sanitizeBase64String(base64String))
+  return getLaplacianAnalysis(src)
+}
+
+export function blobUrl(b64Data) {
+  const blob = b64toBlob(b64Data);
+  const src = URL.createObjectURL(blob)
+
+  getLaplacianAnalysis(src)
+  return src
+
+};
+
+function b64toBlob(b64Data, contentType = "image/png", sliceSize = 512) {
+  const byteCharacters = atob(b64Data);
+  const byteArrays = [];
+
+  for (
+    let offset = 0;
+    offset < byteCharacters.length;
+    offset += sliceSize
+  ) {
+    const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+    const byteNumbers = new Array(slice.length);
+    for (let i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i);
+    }
+
+    const byteArray = new Uint8Array(byteNumbers);
+    byteArrays.push(byteArray);
+  }
+
+  const blob = new Blob(byteArrays, { type: contentType });
+  return blob;
+};
+
+export function getLaplacianAnalysis(src) {
+  const image = new Image();
+  image.src = src
+
+  image.onload = function () {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    canvas.width = image.width;
+    canvas.height = image.height;
+    ctx.drawImage(image, 0, 0);
+
+    const imageData = ctx.getImageData(
+      0,
+      0,
+      canvas.width,
+      canvas.height
+    );
+    const data = imageData.data;
+    const len = data.length;
+    let sum = 0;
+
+    // Convert image to grayscale and calculate sum of pixel values
+    for (let i = 0; i < len; i += 4) {
+      const r = data[i];
+      const g = data[i + 1];
+      const b = data[i + 2];
+      const avg = Math.floor((r + g + b) / 3);
+      data[i] = data[i + 1] = data[i + 2] = avg;
+      sum += avg;
+    }
+
+    // Calculate average brightness
+    const avgBrightness = sum / (len / 4);
+
+    // Calculate variance of Laplacian
+    let variance = 0;
+    for (let i = 0; i < len; i += 4) {
+      const laplacian = Math.abs(data[i] - avgBrightness);
+      variance += laplacian * laplacian;
+    }
+    variance /= len / 4;
+
+    // Define a threshold for blurriness
+    const blurThreshold = 1000; // the higher the better quality will be required
+
+    // Display the result
+    if (variance < blurThreshold) {
+      console.log(111, "The image is blurry.");
+    } else {
+      console.log(111,  "The image is sharp.");
+    }
+  };
+}
+
+function sanitizeBase64String(base64String) {
+  if (base64String.includes('data:image/webp;base64,')) {
+    return base64String.replace('data:image/webp;base64,', '')
+  }
+  return base64String
+}
+
+export function isWithinCirclePerimeter(prediction, canvasTopY, allowedTopY, allowedRightX, allowedBottomY, allowedLeftX, canvasLeftX ) {
+  if (prediction?.topLeft && prediction.bottomRight) {
+
+    if (prediction.topLeft[0] + canvasLeftX >= allowedLeftX // Left X
+      && prediction.bottomRight[0] + canvasLeftX <= allowedRightX // Right X
+      && prediction.topLeft[1] + canvasTopY >= allowedTopY // Top Y
+      && prediction.bottomRight[1] <= allowedBottomY) { // Bottom Y
+        return true 
+    } 
+  } 
+
+  return false
+}
